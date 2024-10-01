325d58210a4483c2ee243c86ce3c0d32d2228f3b
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-02 00:06:42 +0330

    - Change About Form Size

--- LucaToys/Forms/About.Designer.cs
+++ LucaToys/Forms/About.Designer.cs
@@ -34,7 +34,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(500, 595);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "About";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "About";
