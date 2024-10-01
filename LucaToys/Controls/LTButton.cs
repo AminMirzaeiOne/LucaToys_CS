ca623a82da04988744fae0ac579686efe017496a
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-02 00:39:45 +0330

    - Create Symbol Properties In LTButton Control

--- LucaToys/Controls/LTButton.cs
+++ LucaToys/Controls/LTButton.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LucaToys.Controls
 {
-    internal class LTButton
+    public class LTButton : System.Windows.Forms.Button
     {
+        [Category("Symbol")]
+        public System.String SymbolIcon { get; set; } = "";
+        public System.Drawing.Color SymbolColor { get; set; } = Color.Plum;
+        public System.Drawing.Color SymbolHoverColor { get; set; } = Color.White;
+        public System.Byte SymbolSize { get; set; } = 9;
     }
 }
