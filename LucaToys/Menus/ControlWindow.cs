1513e46a1a513f63fbf7c4e0c5a912531ffafe00
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-02 01:38:24 +0330

    - Create Enable Buttons Properties

--- LucaToys/Menus/ControlWindow.cs
+++ LucaToys/Menus/ControlWindow.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,7 +16,34 @@ namespace LucaToys.Menus
         public ControlWindow()
         {
             InitializeComponent();
+            //for(int i = 0; i < 2; i++)
+            //{
+            //    this.Controls.Clear();
+            //    InitializeComponent();
+            //    Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("fa");
+            //}
         }
+
+        [Category("Buttons Enable")]
+        public System.Boolean CloseEnable { get { return this.btnClose.Enabled; } set { this.btnClose.Enabled = value; } }
+
+        [Category("Buttons Enable")]
+        public System.Boolean MaxResEnable { get { return this.btnMaxRes.Enabled; } set { this.btnMaxRes.Enabled = value; } }
+
+        [Category("Buttons Enable")]
+        public System.Boolean MinimizeEnable { get { return this.btnMinimize.Enabled; } set { this.btnMinimize.Enabled = value; } }
+
+        [Category("Buttons Enable")]
+        public System.Boolean HelpEnable { get { return this.btnHelp.Enabled; } set { this.btnHelp.Enabled = value; } }
+
+        [Category("Buttons Enable")]
+        public System.Boolean AboutEnable { get { return this.btnAbout.Enabled; } set { this.btnAbout.Enabled = value; } }
+
+        [Category("Buttons Enable")]
+        public System.Boolean ExitEnable { get { return this.btnExit.Enabled; } set { this.btnExit.Enabled = value; } }
+
+        [Category("Buttons Enable")]
+        public System.Boolean RestartEnable { get { return this.btnRestart.Enabled; } set { this.btnRestart.Enabled = value; } }
 
         private void btnExit_Click(object sender, EventArgs e)
         {
