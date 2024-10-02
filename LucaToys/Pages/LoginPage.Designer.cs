f456a9faaa173d6a4e87b7c151e28ca1d66d33e5
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-02 17:16:27 +0330

    - Add Persian Language To Login Page

--- LucaToys/Pages/LoginPage.Designer.cs
+++ LucaToys/Pages/LoginPage.Designer.cs
@@ -28,55 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginPage));
             this.button1 = new System.Windows.Forms.Button();
-            this.btnLogin = new System.Windows.Forms.Button();
             this.ltTextBox2 = new LucaToys.Controls.LTTextBox();
             this.ltTextBox1 = new LucaToys.Controls.LTTextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
+            resources.ApplyResources(this.button1, "button1");
             this.button1.BackColor = System.Drawing.Color.Transparent;
             this.button1.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.button1.FlatAppearance.BorderSize = 2;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold);
             this.button1.ForeColor = System.Drawing.Color.Violet;
-            this.button1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.button1.Location = new System.Drawing.Point(97, 350);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(214, 40);
-            this.button1.TabIndex = 31;
-            this.button1.Text = "Forget";
-            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.button1.UseVisualStyleBackColor = false;
-            // 
-            // btnLogin
-            // 
-            this.btnLogin.BackColor = System.Drawing.Color.Plum;
-            this.btnLogin.Cursor = System.Windows.Forms.Cursors.Arrow;
-            this.btnLogin.FlatAppearance.BorderColor = System.Drawing.Color.Violet;
-            this.btnLogin.FlatAppearance.BorderSize = 2;
-            this.btnLogin.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Orchid;
-            this.btnLogin.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Violet;
-            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnLogin.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold);
-            this.btnLogin.ForeColor = System.Drawing.Color.White;
-            this.btnLogin.Image = global::LucaToys.Properties.Resources.icons8_login_38;
-            this.btnLogin.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.btnLogin.Location = new System.Drawing.Point(97, 278);
-            this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(214, 64);
-            this.btnLogin.TabIndex = 32;
-            this.btnLogin.Text = " Login App";
-            this.btnLogin.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btnLogin.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.btnLogin.UseVisualStyleBackColor = false;
             // 
             // ltTextBox2
             // 
+            resources.ApplyResources(this.ltTextBox2, "ltTextBox2");
             this.ltTextBox2.BackColor = System.Drawing.Color.White;
             this.ltTextBox2.BorderColor = System.Drawing.Color.Plum;
             this.ltTextBox2.BorderDownColor = System.Drawing.Color.Orchid;
@@ -87,13 +60,11 @@
             this.ltTextBox2.HolderTextColor = System.Drawing.Color.Gray;
             this.ltTextBox2.HolderTextContent = "Enter Password";
             this.ltTextBox2.HolderTextFont = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ltTextBox2.Location = new System.Drawing.Point(51, 224);
             this.ltTextBox2.Name = "ltTextBox2";
-            this.ltTextBox2.Size = new System.Drawing.Size(306, 36);
-            this.ltTextBox2.TabIndex = 29;
             // 
             // ltTextBox1
             // 
+            resources.ApplyResources(this.ltTextBox1, "ltTextBox1");
             this.ltTextBox1.BackColor = System.Drawing.Color.White;
             this.ltTextBox1.BorderColor = System.Drawing.Color.Plum;
             this.ltTextBox1.BorderDownColor = System.Drawing.Color.Orchid;
@@ -104,24 +75,32 @@
             this.ltTextBox1.HolderTextColor = System.Drawing.Color.Gray;
             this.ltTextBox1.HolderTextContent = "Enter Username";
             this.ltTextBox1.HolderTextFont = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ltTextBox1.Location = new System.Drawing.Point(51, 167);
             this.ltTextBox1.Name = "ltTextBox1";
-            this.ltTextBox1.Size = new System.Drawing.Size(306, 36);
-            this.ltTextBox1.TabIndex = 30;
+            // 
+            // btnLogin
+            // 
+            resources.ApplyResources(this.btnLogin, "btnLogin");
+            this.btnLogin.BackColor = System.Drawing.Color.Plum;
+            this.btnLogin.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.btnLogin.FlatAppearance.BorderColor = System.Drawing.Color.Violet;
+            this.btnLogin.FlatAppearance.BorderSize = 2;
+            this.btnLogin.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Orchid;
+            this.btnLogin.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Violet;
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Image = global::LucaToys.Properties.Resources.icons8_login_38;
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.UseVisualStyleBackColor = false;
             // 
             // pictureBox1
             // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
             this.pictureBox1.Image = global::LucaToys.Properties.Resources.icons8_manager_96;
-            this.pictureBox1.Location = new System.Drawing.Point(142, 18);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(120, 120);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 28;
             this.pictureBox1.TabStop = false;
             // 
             // LoginPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.Transparent;
             this.Controls.Add(this.button1);
@@ -130,7 +109,6 @@
             this.Controls.Add(this.ltTextBox1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "LoginPage";
-            this.Size = new System.Drawing.Size(408, 408);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
