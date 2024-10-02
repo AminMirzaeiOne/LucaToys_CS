6cfd17042a5559b3f4f16a88ef700ecd843e8bcc
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-02 15:31:09 +0330

    - Create Border Appearance Properties In LTTextBox Control

--- LucaToys/Controls/LTTextBox.cs
+++ LucaToys/Controls/LTTextBox.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LucaToys.Controls
 {
-    public class LTTextBox
+    public class LTTextBox : System.Windows.Forms.TextBox
     {
+        [Category("Border Appearance")]
+        public System.Byte BorderSize { get; set; } = 2;
+
+        [Category("Border Appearance")]
+        public System.Drawing.Color BorderColor { get; set; } = System.Drawing.Color.Plum;
+        
+        [Category("Border Appearance")]
+        public System.Drawing.Color BorderHoverColor { get; set; } = System.Drawing.Color.Violet;
+
+        [Category("Border Appearance")]
+        public System.Drawing.Color BorderDownColor { get; set; } = System.Drawing.Color.Orchid;
     }
 }
