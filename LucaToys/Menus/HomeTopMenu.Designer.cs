8bbe17af35d4b61be68fd808ec71fac07fdff05e
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-02 21:37:27 +0330

    - Create LTCheckBox Control

--- LucaToys/Menus/HomeTopMenu.Designer.cs
+++ LucaToys/Menus/HomeTopMenu.Designer.cs
@@ -28,18 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Appearance = System.Windows.Forms.Appearance.Button;
+            this.radioButton1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.radioButton1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.radioButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton1.ForeColor = System.Drawing.Color.Coral;
+            this.radioButton1.Location = new System.Drawing.Point(0, 0);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 66);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radioButton1.UseVisualStyleBackColor = false;
             // 
             // HomeTopMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.radioButton1);
             this.Name = "HomeTopMenu";
-            this.Size = new System.Drawing.Size(783, 178);
+            this.Size = new System.Drawing.Size(783, 66);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
     }
 }
