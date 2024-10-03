074489f57c854851e9e55f138ddb4bdb296930d8
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-03 17:11:25 +0330

    - Create Help Submenu In Submenus Folder

---
+++ LucaToys/Submenus/HelpSubmenu.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace LucaToys.Submenus
+{
+    partial class HelpSubmenu
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // HelpSubmenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.White;
+            this.Name = "HelpSubmenu";
+            this.Size = new System.Drawing.Size(295, 351);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ LucaToys/Submenus/HelpSubmenu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LucaToys.Submenus
+{
+    public partial class HelpSubmenu : UserControl
+    {
+        public HelpSubmenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
