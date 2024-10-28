2f0a965650a4113aecc642bd429cf706499835a0
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-10-29 00:31:22 +0330

    - Update SellerUpdate Form Ui

--- LucaToys/Forms/UpdateSeller.Designer.cs
+++ LucaToys/Forms/UpdateSeller.Designer.cs
@@ -35,13 +35,6 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.button6 = new System.Windows.Forms.Button();
-            this.label8 = new System.Windows.Forms.Label();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label6 = new System.Windows.Forms.Label();
-            this.button4 = new System.Windows.Forms.Button();
-            this.label5 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
@@ -57,6 +50,10 @@
             this.btnMaximize = new System.Windows.Forms.Button();
             this.btnClose = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.ltTextBox2 = new LucaToys.Controls.LTTextBox();
+            this.ltTextBox3 = new LucaToys.Controls.LTTextBox();
+            this.ltTextBox4 = new LucaToys.Controls.LTTextBox();
+            this.ltTextBox5 = new LucaToys.Controls.LTTextBox();
             this.tableLayoutPanel1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.panel3.SuspendLayout();
@@ -150,7 +147,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(138, 45);
             this.button2.TabIndex = 48;
-            this.button2.Text = "Delete";
+            this.button2.Text = "Update";
             this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.button2.UseVisualStyleBackColor = false;
             // 
@@ -158,13 +155,10 @@
             // 
             this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox2.Controls.Add(this.button6);
-            this.groupBox2.Controls.Add(this.label8);
-            this.groupBox2.Controls.Add(this.button5);
-            this.groupBox2.Controls.Add(this.button1);
-            this.groupBox2.Controls.Add(this.label6);
-            this.groupBox2.Controls.Add(this.button4);
-            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.ltTextBox5);
+            this.groupBox2.Controls.Add(this.ltTextBox4);
+            this.groupBox2.Controls.Add(this.ltTextBox3);
+            this.groupBox2.Controls.Add(this.ltTextBox2);
             this.groupBox2.Controls.Add(this.pictureBox1);
             this.groupBox2.Font = new System.Drawing.Font("Segoe MDL2 Assets", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox2.ForeColor = System.Drawing.Color.Plum;
@@ -173,111 +167,12 @@
             this.groupBox2.Size = new System.Drawing.Size(301, 425);
             this.groupBox2.TabIndex = 56;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = " Quick Info";
-            // 
-            // button6
-            // 
-            this.button6.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.button6.FlatAppearance.BorderSize = 0;
-            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button6.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.ForeColor = System.Drawing.Color.Plum;
-            this.button6.Location = new System.Drawing.Point(127, 344);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(156, 46);
-            this.button6.TabIndex = 58;
-            this.button6.Text = "09124395950";
-            this.button6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button6.UseVisualStyleBackColor = true;
-            // 
-            // label8
-            // 
-            this.label8.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(39, 353);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(82, 28);
-            this.label8.TabIndex = 1;
-            this.label8.Text = "Phone :";
-            // 
-            // button5
-            // 
-            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.button5.FlatAppearance.BorderSize = 0;
-            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button5.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.ForeColor = System.Drawing.Color.Plum;
-            this.button5.Location = new System.Drawing.Point(6, 283);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(281, 46);
-            this.button5.TabIndex = 58;
-            this.button5.Text = "mohamadrezaei@gmail.com";
-            this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.Plum;
-            this.button1.Location = new System.Drawing.Point(139, 221);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(156, 46);
-            this.button1.TabIndex = 58;
-            this.button1.Text = "Amin Mirzaei";
-            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // label6
-            // 
-            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(21, 230);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(115, 28);
-            this.label6.TabIndex = 1;
-            this.label6.Text = "Full Name :";
-            // 
-            // button4
-            // 
-            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.button4.FlatAppearance.BorderSize = 0;
-            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button4.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.ForeColor = System.Drawing.Color.Plum;
-            this.button4.Location = new System.Drawing.Point(167, 169);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(70, 46);
-            this.button4.TabIndex = 58;
-            this.button4.Text = "329";
-            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // label5
-            // 
-            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(57, 178);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(108, 28);
-            this.label5.TabIndex = 1;
-            this.label5.Text = "Usercode :";
+            this.groupBox2.Text = " Information";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.BackgroundImage = global::LucaToys.Properties.Resources.icons8_male_female_user_group_96;
+            this.pictureBox1.BackgroundImage = global::LucaToys.Properties.Resources.icons8_manager_96;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox1.Location = new System.Drawing.Point(92, 44);
@@ -494,6 +389,74 @@
             this.label1.TabIndex = 43;
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
+            // ltTextBox2
+            // 
+            this.ltTextBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.ltTextBox2.BorderColor = System.Drawing.Color.Plum;
+            this.ltTextBox2.BorderDownColor = System.Drawing.Color.Orchid;
+            this.ltTextBox2.BorderHoverColor = System.Drawing.Color.Violet;
+            this.ltTextBox2.BorderSize = ((byte)(2));
+            this.ltTextBox2.EnableClearButton = true;
+            this.ltTextBox2.FontOptions = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ltTextBox2.HolderTextColor = System.Drawing.Color.Gray;
+            this.ltTextBox2.HolderTextContent = "Usercode";
+            this.ltTextBox2.HolderTextFont = new System.Drawing.Font("Segoe UI", 10F);
+            this.ltTextBox2.Location = new System.Drawing.Point(24, 174);
+            this.ltTextBox2.Name = "ltTextBox2";
+            this.ltTextBox2.Size = new System.Drawing.Size(250, 37);
+            this.ltTextBox2.TabIndex = 59;
+            // 
+            // ltTextBox3
+            // 
+            this.ltTextBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.ltTextBox3.BorderColor = System.Drawing.Color.Plum;
+            this.ltTextBox3.BorderDownColor = System.Drawing.Color.Orchid;
+            this.ltTextBox3.BorderHoverColor = System.Drawing.Color.Violet;
+            this.ltTextBox3.BorderSize = ((byte)(2));
+            this.ltTextBox3.EnableClearButton = true;
+            this.ltTextBox3.FontOptions = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ltTextBox3.HolderTextColor = System.Drawing.Color.Gray;
+            this.ltTextBox3.HolderTextContent = "Name";
+            this.ltTextBox3.HolderTextFont = new System.Drawing.Font("Segoe UI", 10F);
+            this.ltTextBox3.Location = new System.Drawing.Point(24, 231);
+            this.ltTextBox3.Name = "ltTextBox3";
+            this.ltTextBox3.Size = new System.Drawing.Size(250, 37);
+            this.ltTextBox3.TabIndex = 59;
+            // 
+            // ltTextBox4
+            // 
+            this.ltTextBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.ltTextBox4.BorderColor = System.Drawing.Color.Plum;
+            this.ltTextBox4.BorderDownColor = System.Drawing.Color.Orchid;
+            this.ltTextBox4.BorderHoverColor = System.Drawing.Color.Violet;
+            this.ltTextBox4.BorderSize = ((byte)(2));
+            this.ltTextBox4.EnableClearButton = true;
+            this.ltTextBox4.FontOptions = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ltTextBox4.HolderTextColor = System.Drawing.Color.Gray;
+            this.ltTextBox4.HolderTextContent = "Family";
+            this.ltTextBox4.HolderTextFont = new System.Drawing.Font("Segoe UI", 10F);
+            this.ltTextBox4.Location = new System.Drawing.Point(24, 289);
+            this.ltTextBox4.Name = "ltTextBox4";
+            this.ltTextBox4.Size = new System.Drawing.Size(250, 37);
+            this.ltTextBox4.TabIndex = 59;
+            // 
+            // ltTextBox5
+            // 
+            this.ltTextBox5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.ltTextBox5.BorderColor = System.Drawing.Color.Plum;
+            this.ltTextBox5.BorderDownColor = System.Drawing.Color.Orchid;
+            this.ltTextBox5.BorderHoverColor = System.Drawing.Color.Violet;
+            this.ltTextBox5.BorderSize = ((byte)(2));
+            this.ltTextBox5.EnableClearButton = true;
+            this.ltTextBox5.FontOptions = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ltTextBox5.HolderTextColor = System.Drawing.Color.Gray;
+            this.ltTextBox5.HolderTextContent = "Phone";
+            this.ltTextBox5.HolderTextFont = new System.Drawing.Font("Segoe UI", 10F);
+            this.ltTextBox5.Location = new System.Drawing.Point(24, 349);
+            this.ltTextBox5.Name = "ltTextBox5";
+            this.ltTextBox5.Size = new System.Drawing.Size(250, 37);
+            this.ltTextBox5.TabIndex = 59;
+            // 
             // UpdateSeller
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
@@ -512,7 +475,6 @@
             this.panel1.PerformLayout();
             this.panel3.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.groupBox1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -531,13 +493,6 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.DataGridView dataGridView1;
@@ -553,5 +508,9 @@
         private System.Windows.Forms.Button btnMaximize;
         private System.Windows.Forms.Button btnClose;
         private System.Windows.Forms.Label label1;
+        private Controls.LTTextBox ltTextBox2;
+        private Controls.LTTextBox ltTextBox5;
+        private Controls.LTTextBox ltTextBox4;
+        private Controls.LTTextBox ltTextBox3;
     }
 }
