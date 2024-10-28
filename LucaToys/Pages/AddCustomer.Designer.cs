546bb573281bf85bea3591e660a769b18d4ed03f
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-10-28 21:10:47 +0330

    - Create Add Customer Page In Pages Folder

---
+++ LucaToys/Pages/AddCustomer.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace LucaToys.Pages
+{
+    partial class AddCustomer
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // AddCustomer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.Name = "AddCustomer";
+            this.Size = new System.Drawing.Size(1024, 469);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
