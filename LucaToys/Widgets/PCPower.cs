de4ee0763bd5e670b4b64bb640bec47c19afccb1
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-11-03 21:33:02 +0330

    - Update PCPower Widget Ui

--- LucaToys/Widgets/PCPower.cs
+++ LucaToys/Widgets/PCPower.cs
@@ -16,5 +16,10 @@ namespace LucaToys.Widgets
         {
             InitializeComponent();
         }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
     }
 }
