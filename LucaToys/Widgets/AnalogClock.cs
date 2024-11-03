a8c969581e396b4b35f4b57a586a0965814982e4
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-11-03 19:26:09 +0330

    - Add Border To AnalogClock Widget

--- LucaToys/Widgets/AnalogClock.cs
+++ LucaToys/Widgets/AnalogClock.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace LucaToys.Widgets
 {
-    public partial class AnalogClock : Widgets.Widget
+    public partial class AnalogClock : System.Windows.Forms.UserControl 
     {
         public AnalogClock()
         {
