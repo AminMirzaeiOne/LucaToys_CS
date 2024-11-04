bfe9386b5ce592fd9d97409a4a11830b78e45f48
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-11-04 11:42:26 +0330

    - Update Widgets Page Ui

--- LucaToys/Pages/WidgetsPage.Designer.cs
+++ LucaToys/Pages/WidgetsPage.Designer.cs
@@ -28,8 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.SuspendLayout();
+            // 
+            // WidgetsPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.Name = "WidgetsPage";
+            this.Size = new System.Drawing.Size(1344, 460);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
