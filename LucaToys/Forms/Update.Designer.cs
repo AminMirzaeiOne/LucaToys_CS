b0cdeebe7d72b1ee5e2c7e70fbe7926065f25807
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-11-04 23:49:59 +0330

    - Add Move Option To Update Form

--- LucaToys/Forms/Update.Designer.cs
+++ LucaToys/Forms/Update.Designer.cs
@@ -78,6 +78,9 @@
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(830, 36);
             this.panel2.TabIndex = 3;
+            this.panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseDown);
+            this.panel2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseMove);
+            this.panel2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseUp);
             // 
             // btnControl
             // 
