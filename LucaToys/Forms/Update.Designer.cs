eac87a101e56ceffacf4bc6fdbbdbdb0952bc201
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-11-04 23:48:43 +0330

    - Add Maximize and Restore Action To Update Form

--- LucaToys/Forms/Update.Designer.cs
+++ LucaToys/Forms/Update.Designer.cs
@@ -138,6 +138,7 @@
             this.btnMaximize.TabIndex = 49;
             this.btnMaximize.Text = "";
             this.btnMaximize.UseVisualStyleBackColor = false;
+            this.btnMaximize.Click += new System.EventHandler(this.btnMaximize_Click);
             // 
             // btnClose
             // 
