9cf143370d8dd7c041254935c04c0ea9c600ae9f
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-11-05 17:57:26 +0330

    - Update HomeTopMenu

--- LucaToys/Menus/HomeTopMenu.cs
+++ LucaToys/Menus/HomeTopMenu.cs
@@ -26,6 +26,7 @@ namespace LucaToys.Menus
             this.accountingSubmenu = new Submenus.AccountingSubmenu(this.button5);
             this.financeSubmenu = new Submenus.FinanceSubmenu(this.button7);
             this.documents = new Submenus.Documents(this.button21);
+            this.invoice = new Submenus.Invoice(this.button23);
         }
         private LucaToys.Submenus.UsersSubmenu usersSubmenu;
         private LucaToys.Submenus.ProductsSubmenu productsSubmenu;
@@ -38,6 +39,7 @@ namespace LucaToys.Menus
         private LucaToys.Submenus.AccountingSubmenu accountingSubmenu;
         private LucaToys.Submenus.FinanceSubmenu financeSubmenu;
         private LucaToys.Submenus.Documents documents;
+        private LucaToys.Submenus.Invoice invoice;
 
         private void btnMaxRes_Click(object sender, EventArgs e)
         {
