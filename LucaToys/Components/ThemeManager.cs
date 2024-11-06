cbc0ea02155e6a790d4f006589fae6a61153eb22
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-11-07 01:43:54 +0330

    - Create ThemeManager Component

---
+++ LucaToys/Components/ThemeManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucaToys.Components
+{
+    public partial class ThemeManager : Component
+    {
+        public ThemeManager()
+        {
+            InitializeComponent();
+        }
+
+        public ThemeManager(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
